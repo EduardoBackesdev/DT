39aefb423218a046457c34c98ea103c74ef9a1e3
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-12-02 08:39:24 -0300

    att
    att

--- src/components/privatePages/menu/menu.tsx
+++ src/components/privatePages/menu/menu.tsx
@@ -2,8 +2,10 @@ import { useQuery } from "@tanstack/react-query"
 import { getContacts, getFavorites } from "../../../../apis/apisCalls"
 import { useState } from "react"
 import { Home } from "./home/home"
+import { MyRegister } from "./myRegister/myRegister"
 
 export function Menu(){
+    const [nav, setNav] = useState(0)
     const [dados, setDados] = useState(localStorage.getItem('id'))
     const {data:favorites, isLoading: isLoadingFavorites, refetch} = useQuery({
         queryKey: ['getFavorites'],
@@ -13,18 +15,26 @@ export function Menu(){
         queryKey: ["getContacts"], 
         queryFn: () => getContacts(dados)
     })
+    const renderPage = (nav: any) => {
+        switch (nav) {
+            case 0:
+                return <Home dataContatos={contacts} dataFavoritos={favorites} re={refetch} />;
+            case 1:
+                return <MyRegister />;
+        }
+    };
     return isLoading || isLoadingFavorites ? <h2>Carregando...</h2> : (
         <div className='bg-[#d48274] h-screen w-full'>
             <header className="flex justify-around font-bold text-[#ffffff] text-2xl pb-3">
-                <h2>Home</h2>
-                <h2>Meu Cadastro</h2>
-                <h2>Usuários</h2>
-                <h2>Pessoas</h2>
-                <h2>Contatos</h2>
-                <h2>Logout</h2>
+                <h2 className="cursor-pointer" onClick={() => { setNav(0) }}>Home</h2>
+                <h2 className="cursor-pointer" onClick={() => { setNav(1) }}>Meu Cadastro</h2>
+                <h2 className="cursor-pointer" onClick={() => { setNav(2) }}>Usuários</h2>
+                <h2 className="cursor-pointer" onClick={() => { setNav(3) }}>Pessoas</h2>
+                <h2 className="cursor-pointer" onClick={() => { setNav(4) }}>Contatos</h2>
+                <h2 className="cursor-pointer" onClick={() => { setNav(5) }}>Logout</h2>
             </header>
             <div className="bg-[#ebeaea] h-[90vh]"> 
-                   <Home dataContatos={contacts} dataFavoritos={favorites} re={refetch} />
+                {renderPage(nav)}
             </div>
         </div>
     )
---
+++ src/components/privatePages/menu/myRegister/myRegister.tsx
@@ -0,0 +1,7 @@
+export function MyRegister() {
+    return (
+        <div className="w-full">
+            <h2>Ola mundo</h2>
+        </div>
+    )
+}
