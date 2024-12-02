07a3269c7b828d6ed02e1f71c900b17613e6d565
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-12-02 09:38:36 -0300

    att

--- src/components/privatePages/menu/myRegister/myRegister.tsx
+++ src/components/privatePages/menu/myRegister/myRegister.tsx
@@ -1,7 +1,22 @@
 export function MyRegister() {
     return (
         <div className="w-full">
-            <h2>Ola mundo</h2>
+            <div className=" text-3xl font-bold text-[#666464]">
+                <div className="flex justify-center text-3xl font-bold text-[#666464]">
+                    <h2>Atualize seus dados cadastrais</h2>
+                </div>
+                <div className="flex justify-center">
+                    <p className="text-lg">Preencha o formulário</p>
+                    <div className="pl-2 text-2xl">
+                    </div>
+                </div>
+            </div>
+            <div>
+                <form>
+
+                </form>
+            </div>
+
         </div>
     )
 }
